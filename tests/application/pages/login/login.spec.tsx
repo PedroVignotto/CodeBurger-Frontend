f7560b4f2b8d49584c601c0639fdf3d4963aba2c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 20:23:31 -0300

    test: ensure Login call validation with correct email

--- tests/application/pages/login/login.spec.tsx
+++ tests/application/pages/login/login.spec.tsx
@@ -1,13 +1,29 @@
+import { accountParams } from '@/../tests/mocks'
 import { Login } from '@/application/pages'
+import { Validator } from '@/application/validation'
 
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { mock } from 'jest-mock-extended'
 import React from 'react'
 
 describe('Login', () => {
-  it('Should start with initial states', () => {
-    render(<Login />)
+  const { email } = accountParams
 
+  const validator = mock<Validator>()
+
+  beforeEach(() => {
+    render(<Login validation={validator} />)
+  })
+
+  it('Should start with initial states', () => {
     expect(screen.getByText('Login')).toBeTruthy()
     expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  it('Should call validation with correct email', () => {
+    fireEvent.input(screen.getByLabelText('Email'), { target: { value: email } })
+
+    expect(validator.validate).toHaveBeenCalledWith({ email })
+    expect(validator.validate).toHaveBeenCalledTimes(2)
   })
 })
