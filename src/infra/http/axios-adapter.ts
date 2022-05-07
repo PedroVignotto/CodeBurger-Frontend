2223494addb3899158f9620ef4249169a6ed62c2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 09:04:26 -0300

    feat: ensure AxiosAdapter return correct response

--- src/infra/http/axios-adapter.ts
+++ src/infra/http/axios-adapter.ts
@@ -1,9 +1,11 @@
-import { HttpRequest } from '@/domain/contracts/http'
+import { HttpClient, HttpRequest, HttpResponse } from '@/domain/contracts/http'
 
 import axios from 'axios'
 
-export class AxiosAdapter {
-  async request ({ url, method, body, headers }: HttpRequest): Promise<void> {
-    await axios.request({ url, method, data: body, headers })
+export class AxiosAdapter implements HttpClient {
+  async request ({ url, method, body, headers }: HttpRequest): Promise<HttpResponse> {
+    const { status, data } = await axios.request({ url, method, data: body, headers })
+
+    return { statusCode: status, data }
   }
 }
