0cdd63d24f4d471c89ac4f9ce534b23331d8d836
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 20:25:14 -0300

    feat: ensure Login call validation with correct password

--- src/application/pages/login/index.tsx
+++ src/application/pages/login/index.tsx
@@ -11,10 +11,15 @@ type Props = { validation: Validator }
 export const Login: React.FC<Props> = ({ validation }) => {
   const [loading] = useState(false)
   const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     validation.validate({ email })
   }, [email])
+
+  useEffect(() => {
+    validation.validate({ password })
+  }, [password])
 
   return (
     <Container>
@@ -25,7 +30,7 @@ export const Login: React.FC<Props> = ({ validation }) => {
           <img src={logo} alt="Code-burguer" />
           <form>
             <Input type="text" name="email" placeholder="Email" setState={setEmail} />
-            <Input type="password" name="password" placeholder="Senha" />
+            <Input type="password" name="password" placeholder="Senha" setState={setPassword} />
             <button type="submit" disabled>{ loading ? <Spinner /> : 'Login' }</button>
           </form>
           <a href='#'>Não tem uma conta? <span>Crie uma!</span></a>
