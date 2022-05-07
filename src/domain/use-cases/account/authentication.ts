0e2807001b13929325797b280a72dde96a9321da
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 07:13:37 -0300

    feat: ensure AuthenticationUseCase return an account if HttpClient returns 200

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@/domain/contracts/http'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { Account } from '@/domain/models'
 
-type Setup = (url: string, httpClient: HttpClient) => Authentication
+type Setup = (url: string, httpClient: HttpClient<Account>) => Authentication
 type Input = { email: string, password: string }
-type Output = undefined | Error
+type Output = { name: string, accessToken: string } | Error
 export type Authentication = (input: Input) => Promise<Output>
 
 export const authenticationUseCase: Setup = (url, httpClient) => async (input) => {
-  const httpResponse = await httpClient.request({ url, method: 'post', body: input })
+  const { statusCode, data } = await httpClient.request({ url, method: 'post', body: input })
 
-  switch (httpResponse.statusCode) {
-    case 200: return undefined
+  switch (statusCode) {
+    case 200: return data!
     case 401: throw new InvalidCredentialsError()
     default: throw new UnexpectedError()
   }
