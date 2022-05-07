834329c86036fb8bfcc117c0bdbffd1d5d909e8e
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 09:39:49 -0300

    chore: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const { join } = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: { '@': join(__dirname, 'src') }
+  },
+  devServer: {
+    devMiddleware: { writeToDisk: true },
+    static: { directory: './public' },
+    historyApiFallback: true
+  },
+  externals: { react: 'React', 'react-dom': 'ReactDOM' },
+  plugins: [new CleanWebpackPlugin()]
+}
