dc54767ae9615052e3c5dec67259c563cfaa0c0b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 06:26:22 -0300

    feat: add HttpResponse to HttpClient

--- src/domain/contracts/http/http-client.ts
+++ src/domain/contracts/http/http-client.ts
@@ -1,6 +1,8 @@
-export interface HttpClient {
-  request: (data: HttpRequest) => Promise<void>
+export interface HttpClient<R = any> {
+  request: (data: HttpRequest) => Promise<HttpResponse<R>>
 }
+
+export type HttpResponse<T = any> = { statusCode: number, data?: T }
 
 export type HttpRequest = { url: string, method: HttpMethod, body?: any }
 
