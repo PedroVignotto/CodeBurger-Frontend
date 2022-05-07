48f79336d298fd587f19f0a545b8b865c5f5265d
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 06:35:47 -0300

    feat: ensure AuthenticationUseCase throw UnexpectedError if HttpClient returns 400

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@/domain/contracts/http'
-import { InvalidCredentialsError } from '@/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => Authentication
 type Input = { email: string, password: string }
@@ -10,6 +10,7 @@ export const authenticationUseCase: Setup = (url, httpClient) => async (input) =
   const httpResponse = await httpClient.request({ url, method: 'post', body: input })
 
   switch (httpResponse.statusCode) {
+    case 400: throw new UnexpectedError()
     case 401: throw new InvalidCredentialsError()
     default: return undefined
   }
