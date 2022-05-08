634c6be239479a9e2014e57fa5c87f9bdafd2b69
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-07 22:46:39 -0300

    test: ensure Login enable submit button if form is valid

--- tests/application/pages/login/login.spec.tsx
+++ tests/application/pages/login/login.spec.tsx
@@ -11,32 +11,39 @@ describe('Login', () => {
 
   const validator = mock<Validator>()
 
+  const makeSut = (): void => { render(<Login validation={validator} />) }
+
   beforeAll(() => {
     validator.validate.mockReturnValue('')
   })
 
-  beforeEach(() => {
-    render(<Login validation={validator} />)
-  })
-
   it('Should start with initial states', () => {
+    validator.validate.mockReturnValueOnce(error)
+
+    makeSut()
+
     expect(screen.getByText('Login')).toBeTruthy()
     expect(screen.getByRole('button')).toBeDisabled()
   })
 
   it('Should call validation with correct email', () => {
+    makeSut()
+
     fireEvent.input(screen.getByLabelText('Email'), { target: { value: email } })
 
     expect(validator.validate).toHaveBeenCalledWith('email', email)
   })
 
   it('Should call validation with correct password', () => {
+    makeSut()
+
     fireEvent.input(screen.getByLabelText('Senha'), { target: { value: password } })
 
     expect(validator.validate).toHaveBeenCalledWith('password', password)
   })
 
   it('Should show email error if Validation fails', () => {
+    makeSut()
     validator.validate.mockReturnValueOnce(error)
 
     fireEvent.input(screen.getByLabelText('Email'), { target: { value: email } })
@@ -45,6 +52,7 @@ describe('Login', () => {
   })
 
   it('Should show password error if Validation fails', () => {
+    makeSut()
     validator.validate.mockReturnValueOnce(error)
 
     fireEvent.input(screen.getByLabelText('Senha'), { target: { value: password } })
@@ -53,14 +61,27 @@ describe('Login', () => {
   })
 
   it('Should show valid email state if Validation succeeds', () => {
+    makeSut()
+
     fireEvent.input(screen.getByLabelText('Email'), { target: { value: email } })
 
     expect(screen.getByLabelText('Email')).toHaveProperty('title', '')
   })
 
   it('Should show valid password state if Validation succeeds', () => {
+    makeSut()
+
     fireEvent.input(screen.getByLabelText('Senha'), { target: { value: password } })
 
     expect(screen.getByLabelText('Senha')).toHaveProperty('title', '')
+  })
+
+  it('Should enable submit button if form is valid', () => {
+    makeSut()
+
+    fireEvent.input(screen.getByLabelText('Email'), { target: { value: email } })
+    fireEvent.input(screen.getByLabelText('Senha'), { target: { value: password } })
+
+    expect(screen.getByRole('button')).toBeEnabled()
   })
 })
