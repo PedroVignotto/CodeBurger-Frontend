ce8c01bfdff555e3a58bd5d740c41da6b4686484
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 13:05:52 -0300

    test: ensure Login keep the button disabled if form is invalid

--- tests/e2e/cypress/integration/login.test.ts
+++ tests/e2e/cypress/integration/login.test.ts
@@ -1,9 +1,18 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('Should load with correct initial state', () => {
+    cy.getSubmitButton().should('be.disabled').should('have.text', 'Login')
+  })
+
+  it('Should keep the button disabled if form is invalid', () => {
+    cy.getInputById('email').focus().type(faker.random.word()).should('have.attr', 'title', 'Valor inválido')
+    cy.getInputById('password').focus().type(faker.random.word())
+
     cy.getSubmitButton().should('be.disabled').should('have.text', 'Login')
   })
 })
