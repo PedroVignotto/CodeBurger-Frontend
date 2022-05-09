7fcc4fdba4221e9d35019f017f2c33b891dea669
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-08 21:55:49 -0300

    test: ensure RequiredValidation return falsy if field is not empty

--- tests/application/errors/required.spec.ts
+++ tests/application/errors/required.spec.ts
@@ -1,12 +1,30 @@
 import { RequiredValidation } from '@/application/validation'
 import { RequiredFieldError } from '@/application/validation/errors'
 
+import faker from 'faker'
+
 describe('RequiredValidation', () => {
+  let value: string
+  let fieldName: string
+
+  beforeAll(() => {
+    value = faker.database.column()
+    fieldName = faker.database.column()
+  })
+
   it('Should return error if field is empty', () => {
-    const sut = new RequiredValidation('email')
+    const sut = new RequiredValidation(fieldName)
 
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('Should return falsy if field is not empty', () => {
+    const sut = new RequiredValidation(fieldName)
+
+    const error = sut.validate(value)
+
+    expect(error).toBeFalsy()
   })
 })
