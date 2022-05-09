097c7942c84a5b801db18d49dcca29724b7e13de
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-08 22:53:04 -0300

    test: ensure ValidationBuilder return a Email validation if email() is call

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
+import { EmailValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
 
 import faker from 'faker'
 
@@ -12,6 +12,12 @@ describe('ValidationBuilder', () => {
   it('Should return a Required validation if required() is call', () => {
     const validations = Builder.of(fieldName).required().build()
 
-    expect(validations).toEqual([new RequiredValidation(fieldName)])
+    expect(validations).toStrictEqual([new RequiredValidation(fieldName)])
+  })
+
+  it('Should return a Email validation if email() is call', () => {
+    const validators = Builder.of(fieldName).email().build()
+
+    expect(validators).toStrictEqual([new EmailValidation(fieldName)])
   })
 })
