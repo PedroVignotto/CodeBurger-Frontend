5907450d65836e41ac3c53416a41d60072eb5b71
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 13:10:15 -0300

    test: ensure Login enable the button if form is valid

--- tests/e2e/cypress/integration/login.test.ts
+++ tests/e2e/cypress/integration/login.test.ts
@@ -11,8 +11,15 @@ describe('Login', () => {
 
   it('Should keep the button disabled if form is invalid', () => {
     cy.getInputById('email').focus().type(faker.random.word()).should('have.attr', 'title', 'Valor inválido')
-    cy.getInputById('password').focus().type(faker.random.word())
+    cy.getInputById('password').focus().type(faker.internet.password())
 
     cy.getSubmitButton().should('be.disabled').should('have.text', 'Login')
+  })
+
+  it('Should enable the button if form is valid', () => {
+    cy.getInputById('email').focus().type(faker.internet.email())
+    cy.getInputById('password').focus().type(faker.internet.password())
+
+    cy.getSubmitButton().should('be.enabled').should('have.text', 'Login')
   })
 })
