e4b517636b48e67f934692df12f3762fefcbe6e1
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 20:10:47 -0300

    feat: ensure AddAccountUseCase return an account if HttpClient returns 201

--- src/domain/use-cases/account/add-account.ts
+++ src/domain/use-cases/account/add-account.ts
@@ -4,14 +4,14 @@ import { Account } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<Account>) => AddAccount
 type Input = { name: string, email: string, password: string }
-type Output = void
+type Output = { name: string, accessToken: string }
 export type AddAccount = (input: Input) => Promise<Output>
 
 export const addAccountUseCase: Setup = (url, httpClient) => async (input) => {
-  const { statusCode } = await httpClient.request({ url, method: 'post', body: input })
+  const { statusCode, data } = await httpClient.request({ url, method: 'post', body: input })
 
   switch (statusCode) {
-    case 201: break
+    case 201: return data!
     case 403: throw new FieldInUseError('email')
     default: throw new UnexpectedError()
   }
