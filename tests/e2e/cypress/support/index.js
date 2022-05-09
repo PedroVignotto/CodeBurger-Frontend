228746323e8febdb22111d2d4ecbc3c0b8f7bf17
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 13:05:31 -0300

    chore: add getInputById support method

--- tests/e2e/cypress/support/index.js
+++ tests/e2e/cypress/support/index.js
@@ -1,1 +1,2 @@
 Cypress.Commands.add('getSubmitButton', () => cy.get('button[type=submit]'))
+Cypress.Commands.add('getInputById', (id) => cy.get(`input[id=${id}]`))
--- tests/e2e/cypress/types/index.d.ts
+++ tests/e2e/cypress/types/index.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
   export interface Chainable {
     getSubmitButton: () => Chainable<Element>
+    getInputById: (id: string) => Chainable<Element>
   }
 }
