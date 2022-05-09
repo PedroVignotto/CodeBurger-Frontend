e5b0317b91db802480e68ce714de1de85e117c5a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 01:29:58 -0300

    feat: ensure AxiosAdapter return correct error

--- src/infra/http/axios-adapter.ts
+++ src/infra/http/axios-adapter.ts
@@ -1,11 +1,15 @@
 import { HttpClient, HttpRequest, HttpResponse } from '@/domain/contracts/http'
 
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export class AxiosAdapter implements HttpClient {
   async request ({ url, method, body, headers }: HttpRequest): Promise<HttpResponse> {
-    const { status, data } = await axios.request({ url, method, data: body, headers })
+    let axiosResponse: AxiosResponse
 
-    return { statusCode: status, data }
+    try {
+      axiosResponse = await axios.request({ url, method, data: body, headers })
+    } catch (error: any) { axiosResponse = error.response }
+
+    return { statusCode: axiosResponse.status, data: axiosResponse.data }
   }
 }
