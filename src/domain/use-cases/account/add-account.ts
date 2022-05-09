3431901a373127cd62f40270fcf313e461f9d2d4
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 20:00:46 -0300

    feat: ensure AddAccountUseCase throw EmailInUseError if HttpClient returns 403

--- src/domain/use-cases/account/add-account.ts
+++ src/domain/use-cases/account/add-account.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@/domain/contracts/http'
+import { FieldInUseError } from '@/domain/errors'
 import { Account } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<Account>) => AddAccount
@@ -7,5 +8,10 @@ type Output = void
 export type AddAccount = (input: Input) => Promise<Output>
 
 export const addAccountUseCase: Setup = (url, httpClient) => async (input) => {
-  await httpClient.request({ url, method: 'post', body: input })
+  const { statusCode } = await httpClient.request({ url, method: 'post', body: input })
+
+  switch (statusCode) {
+    case 403: throw new FieldInUseError('email')
+    default: break
+  }
 }
