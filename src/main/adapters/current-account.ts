466ebcf4f0d30f130c598d1775a5848ec7d3c939
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 10:29:51 -0300

    feat: add CurrentAccountAdapter

---
+++ src/main/adapters/current-account.ts
@@ -0,0 +1,5 @@
+import { makeLocalStorageAdapter } from '@/main/factories/infra/cache'
+import { Account } from '@/domain/models'
+
+export const setCurrentAccountAdapter = (account: Account): void =>
+  makeLocalStorageAdapter().set({ key: 'account', value: account })
