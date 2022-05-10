b332e08ea59bd500ae09b510c2b78129acc1a596
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 22:12:47 -0300

    feat: ensure SignUp call validation with correct values

--- src/application/pages/signup/index.tsx
+++ src/application/pages/signup/index.tsx
@@ -1,12 +1,32 @@
 import { signup, logo } from '@/application/assets'
 import { Input, Spinner } from '@/application/components'
+import { Validator } from '@/application/validation'
 
 import { Container, ContentWrap } from './styles'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const SignUp: React.FC = () => {
+type Props = { validation: Validator }
+
+export const SignUp: React.FC<Props> = ({ validation }) => {
   const loading = false
+
+  const [name, setName] = useState('')
+  const [nameError, setNameError] = useState<string | undefined>('')
+  const [email, setEmail] = useState('')
+  const [emailError, setEmailError] = useState<string | undefined>('')
+  const [password, setPassword] = useState('')
+  const [passwordError, setPasswordError] = useState<string | undefined>('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [passwordConfirmationError, setPasswordConfirmationError] = useState<string | undefined>('')
+
+  useEffect(() => setNameError(validation.validate('name', name)), [name])
+
+  useEffect(() => setEmailError(validation.validate('email', email)), [email])
+
+  useEffect(() => setPasswordError(validation.validate('password', password)), [password])
+
+  useEffect(() => setPasswordConfirmationError(validation.validate('passwordConfirmation', passwordConfirmation)), [passwordConfirmation])
 
   return (
     <Container>
@@ -16,10 +36,10 @@ export const SignUp: React.FC = () => {
       <ContentWrap>
           <img src={logo} alt="Code-burguer" />
           <form>
-            <Input type="text" name="name" placeholder="Nome" state={''} setState={''} />
-            <Input type="text" name="email" placeholder="Email" state={''} setState={''} />
-            <Input type="password" name="password" placeholder="Senha" state={''} setState={''} />
-            <Input type="password" name="passwordConfirmation" placeholder="Confirmar senha" state={''} setState={''} />
+            <Input type="text" name="name" placeholder="Nome" state={nameError} setState={setName} />
+            <Input type="text" name="email" placeholder="Email" state={emailError} setState={setEmail} />
+            <Input type="password" name="password" placeholder="Senha" state={passwordError} setState={setPassword} />
+            <Input type="password" name="passwordConfirmation" placeholder="Confirmar senha" state={passwordConfirmationError} setState={setPasswordConfirmation} />
             <button type="submit" disabled={true}>{ loading ? <Spinner /> : 'Cadastre-se' }</button>
           </form>
           <a href="#">Já tem uma conta? <span>Entre!</span></a>
