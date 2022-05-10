de966cd41fcd4e421d5b06df4e36d2c1dbe964b4
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-09 22:13:28 -0300

    test: ensure SignUp call validation with correct values

--- tests/application/pages/signup/signup.spec.tsx
+++ tests/application/pages/signup/signup.spec.tsx
@@ -1,11 +1,25 @@
+import { accountParams, populateField } from '@/tests/mocks'
 import { SignUp } from '@/application/pages'
+import { Validator } from '@/application/validation'
 
 import { render, screen } from '@testing-library/react'
+import { mock } from 'jest-mock-extended'
 import React from 'react'
 
 describe('SignUp', () => {
+  const { name, email, password, passwordConfirmation } = accountParams
+
+  const validator = mock<Validator>()
+
   const makeSut = (): void => {
-    render(<SignUp />)
+    render(<SignUp validation={validator} />)
+  }
+
+  const populateFields = (): void => {
+    populateField('Nome', name)
+    populateField('Email', email)
+    populateField('Senha', password)
+    populateField('Confirmar senha', passwordConfirmation)
   }
 
   it('Should load with correct initial state', () => {
@@ -13,5 +27,16 @@ describe('SignUp', () => {
 
     expect(screen.getByText('Cadastre-se')).toBeTruthy()
     expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  it('Should call validation with correct values', () => {
+    makeSut()
+
+    populateFields()
+
+    expect(validator.validate).toHaveBeenCalledWith('name', name)
+    expect(validator.validate).toHaveBeenCalledWith('email', email)
+    expect(validator.validate).toHaveBeenCalledWith('password', password)
+    expect(validator.validate).toHaveBeenCalledWith('passwordConfirmation', passwordConfirmation)
   })
 })
--- tests/mocks/account-params.ts
+++ tests/mocks/account-params.ts
@@ -1,9 +1,12 @@
 import faker from 'faker'
 
+const password = faker.internet.password(8)
+
 export const accountParams = {
   name: faker.name.findName(),
   email: faker.internet.email(),
-  password: faker.internet.password(8),
+  password,
+  passwordConfirmation: password,
   accessToken: faker.datatype.uuid(),
   error: faker.random.words(3)
 }
