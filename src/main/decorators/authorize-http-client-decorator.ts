f330409f86d7e0d9f4f92acf508dedca797726ce
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-12 18:33:32 -0300

    feat: ensure AuthorizeHttpClientDecorator add headers to HttpClient

--- src/main/decorators/authorize-http-client-decorator.ts
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -5,7 +5,9 @@ export class AuthorizeHttpClientDecorator implements HttpClient {
   constructor (private readonly getStorage: GetStorage, private readonly httpClient: HttpClient) {}
 
   async request (data: HttpRequest): Promise<HttpResponse> {
-    this.getStorage.get({ key: 'account' })
+    const account = this.getStorage.get({ key: 'account' })
+
+    if (account?.accessToken) Object.assign(data, { headers: { authorization: `Bearer: ${account.accessToken as string}` } })
 
     return await this.httpClient.request(data)
   }
