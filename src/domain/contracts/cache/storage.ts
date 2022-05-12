2234a52b8ea128f3d36731d4fb8be4bc6b095a22
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-11 23:03:25 -0300

    feat: add GetStorage interface

--- src/domain/contracts/cache/storage.ts
+++ src/domain/contracts/cache/storage.ts
@@ -6,3 +6,12 @@ export namespace SetStorage {
   export type Input = { key: string, value: object }
   export type Output = void
 }
+
+export interface GetStorage {
+  get: (input: GetStorage.Input) => GetStorage.Output
+}
+
+export namespace GetStorage {
+  export type Input = { key: string }
+  export type Output = any
+}
