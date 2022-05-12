b50f030c68d7c9f5c6b85a0ff934f228f26a10d2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-12 14:32:08 -0300

    test: ensure Menu render categories on success

--- tests/application/pages/menu/menu.spec.tsx
+++ tests/application/pages/menu/menu.spec.tsx
@@ -1,11 +1,18 @@
+import { categoryParams, productParams } from '@/tests/mocks'
 import { Menu } from '@/application/pages'
 
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 
 describe('Menu', () => {
   const listCategories: jest.Mock = jest.fn()
+
+  const { id, name } = categoryParams
+
+  beforeAll(() => {
+    listCategories.mockResolvedValue([{ id, name, products: [productParams] }])
+  })
 
   const makeSut = (): void => {
     render(
@@ -15,16 +22,29 @@ describe('Menu', () => {
     )
   }
 
-  it('Should load with correct initial state', () => {
+  it('Should load with correct initial state', async () => {
+    listCategories.mockResolvedValueOnce([])
+
     makeSut()
 
     expect(screen.getAllByRole('list')).toHaveLength(2)
     expect(screen.getAllByRole('listitem')).toHaveLength(7)
+    await waitFor(() => screen.getByRole('heading', { name: /cardápio/i }))
   })
 
-  it('Should call listCategories', () => {
+  it('Should call listCategories', async () => {
     makeSut()
 
+    await waitFor(() => screen.getByRole('heading', { name: /cardápio/i }))
+
     expect(listCategories).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should render categories on success', async () => {
+    makeSut()
+
+    await waitFor(() => screen.getByRole('list'))
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(1)
   })
 })
