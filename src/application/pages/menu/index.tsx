9d6075f89848a49104acfc766f20472841f00c48
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-12 12:19:28 -0300

    feat: ensure Menu call listCategories

--- src/application/pages/menu/index.tsx
+++ src/application/pages/menu/index.tsx
@@ -1,11 +1,18 @@
 import { Footer, Header } from '@/application/components'
 import { Skeleton } from '@/application/pages/menu/skeleton'
+import { ListCategories } from '@/domain/use-cases/category'
 
 import { Container, Content } from './styles'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export const Menu: React.FC = () => {
+type Props = { listCategories: ListCategories }
+
+export const Menu: React.FC<Props> = ({ listCategories }: Props) => {
+  useEffect(() => {
+    (async () => { await listCategories() })()
+  }, [])
+
   return (
     <Container>
       <Header />
