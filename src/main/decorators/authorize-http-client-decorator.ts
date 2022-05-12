b70782c80b7a9ed8ff1dedfef2a8fdb701645085
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-12 18:21:55 -0300

    feat: ensure AuthorizeHttpClientDecorator not add headers if GetStorage is invalid

--- src/main/decorators/authorize-http-client-decorator.ts
+++ src/main/decorators/authorize-http-client-decorator.ts
@@ -2,11 +2,11 @@ import { HttpClient, HttpRequest, HttpResponse } from '@/domain/contracts/http'
 import { GetStorage } from '@/domain/contracts/cache'
 
 export class AuthorizeHttpClientDecorator implements HttpClient {
-  constructor (private readonly getStorage: GetStorage) {}
+  constructor (private readonly getStorage: GetStorage, private readonly httpClient: HttpClient) {}
 
   async request (data: HttpRequest): Promise<HttpResponse> {
     this.getStorage.get({ key: 'account' })
 
-    return null as any
+    return await this.httpClient.request(data)
   }
 }
