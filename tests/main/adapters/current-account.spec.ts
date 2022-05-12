ffe0d1f54b47450fd3f05359b1ddcd422471217a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-11 23:35:16 -0300

    test: ensure CurrentAccountAdapter call LocalStorageAdapter.get with correct values

--- tests/main/adapters/current-account.spec.ts
+++ tests/main/adapters/current-account.spec.ts
@@ -1,5 +1,5 @@
 import { accountParams } from '@/tests/mocks'
-import { setCurrentAccountAdapter } from '@/main/adapters'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters'
 import { LocalStorageAdapter } from '@/infra/cache'
 
 import { mocked } from 'jest-mock'
@@ -10,12 +10,26 @@ describe('CurrentAccountAdapter', () => {
   const { name, accessToken } = accountParams
 
   const setSpy = jest.fn()
+  const getSpy = jest.fn()
 
-  mocked(LocalStorageAdapter).mockImplementation(jest.fn().mockImplementation(() => ({ set: setSpy })))
+  mocked(LocalStorageAdapter).mockImplementation(jest.fn().mockImplementation(() => ({ set: setSpy, get: getSpy })))
 
-  it('Should call LocalStorageAdapter.set with correct values', () => {
-    setCurrentAccountAdapter({ name, accessToken })
+  describe('set()', () => {
+    it('Should call LocalStorageAdapter.set with correct values', () => {
+      setCurrentAccountAdapter({ name, accessToken })
 
-    expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: { name, accessToken } })
+      expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: { name, accessToken } })
+    })
+  })
+
+  describe('get()', () => {
+    it('Should call LocalStorageAdapter.get with correct values', () => {
+      getSpy.mockReturnValueOnce({ name, accessToken })
+
+      const account = getCurrentAccountAdapter()
+
+      expect(getSpy).toHaveBeenCalledWith({ key: 'account' })
+      expect(account).toEqual({ name, accessToken })
+    })
   })
 })
