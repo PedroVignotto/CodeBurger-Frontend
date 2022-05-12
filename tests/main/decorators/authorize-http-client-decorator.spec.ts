f5b334568c9edc486ff4cea61485836732940682
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-12 18:22:22 -0300

    test: ensure AuthorizeHttpClientDecorator not add headers if GetStorage is invalid

--- tests/main/decorators/authorize-http-client-decorator.spec.ts
+++ tests/main/decorators/authorize-http-client-decorator.spec.ts
@@ -3,6 +3,7 @@ import { AuthorizeHttpClientDecorator } from '@/main/decorators'
 import { GetStorage } from '@/domain/contracts/cache'
 
 import { mock } from 'jest-mock-extended'
+import { HttpClient } from '@/domain/contracts/http'
 
 describe('AuthorizeHttpClientDecorator', () => {
   let sut: AuthorizeHttpClientDecorator
@@ -10,14 +11,21 @@ describe('AuthorizeHttpClientDecorator', () => {
   const { url, method, body, headers } = httpClientParams
 
   const getStorage = mock<GetStorage>()
+  const httpClient = mock<HttpClient>()
 
   beforeEach(() => {
-    sut = new AuthorizeHttpClientDecorator(getStorage)
+    sut = new AuthorizeHttpClientDecorator(getStorage, httpClient)
   })
 
   it('Should call GetStorage with correct value', async () => {
     await sut.request({ url, method, body, headers })
 
     expect(getStorage.get).toHaveBeenCalledWith({ key: 'account' })
+  })
+
+  it('Should not add headers if GetStorage is invalid', async () => {
+    await sut.request({ url, method })
+
+    expect(httpClient.request).toHaveBeenCalledWith({ url, method })
   })
 })
