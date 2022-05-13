1adf24b4eac8dceb4162bfe49a5f28273a81b399
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-13 19:01:48 -0300

    feat: add address model

---
+++ src/domain/models/address.ts
@@ -0,0 +1,9 @@
+export type Address = {
+  id: string
+  surname: string
+  zipCode: string
+  district: string
+  street: string
+  number: number
+  complement?: string
+}
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,3 +1,4 @@
 export * from './account'
 export * from './category'
 export * from './product'
+export * from './address'
