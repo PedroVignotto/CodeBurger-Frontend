d2692949784d6f739309ffc0c3776479345b3ef2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-13 19:13:17 -0300

    feat: ensure ListAddressesUseCase return address data if HttpClient returns 200

--- src/domain/use-cases/address/list-addresses.ts
+++ src/domain/use-cases/address/list-addresses.ts
@@ -3,14 +3,14 @@ import { UnauthorizedError, UnexpectedError } from '@/domain/errors'
 import { Address } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<Address[]>) => ListAddresses
-type Output = void
+type Output = Address[]
 export type ListAddresses = () => Promise<Output>
 
 export const listAddressesUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
 
   switch (statusCode) {
-    case 200: break
+    case 200: return data!
     case 401: throw new UnauthorizedError()
     default: throw new UnexpectedError()
   }
