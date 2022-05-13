b479b7a99f502520a2e4a776f28a426653902873
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-13 19:05:43 -0300

    feat: ensure ListAddressesUseCase throw UnauthorizedError if HttpClient returns 401

--- src/domain/use-cases/address/list-addresses.ts
+++ src/domain/use-cases/address/list-addresses.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@/domain/contracts/http'
+import { UnauthorizedError } from '@/domain/errors'
 import { Address } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<Address[]>) => ListAddresses
@@ -6,5 +7,10 @@ type Output = void
 export type ListAddresses = () => Promise<Output>
 
 export const listAddressesUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+
+  switch (statusCode) {
+    case 200: break
+    default: throw new UnauthorizedError()
+  }
 }
