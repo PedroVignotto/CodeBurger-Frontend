1d95823fccb3d34b5b0bff50f3fc07adb5caf262
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-13 11:50:13 -0300

    feat: ensure Menu logout on UnauthorizedError

--- src/application/pages/menu/index.tsx
+++ src/application/pages/menu/index.tsx
@@ -1,15 +1,21 @@
 import { Error, Footer, Header } from '@/application/components'
 import { Categories } from '@/application/pages/menu/categories'
+import { AccountContext } from '@/application/contexts'
 import { ListCategories } from '@/domain/use-cases/category'
 import { Category } from '@/domain/models'
+import { UnauthorizedError } from '@/domain/errors'
 
 import { Container, Content } from './styles'
 
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 type Props = { listCategories: ListCategories }
 
 export const Menu: React.FC<Props> = ({ listCategories }: Props) => {
+  const navigate = useNavigate()
+  const { setCurrentAccount } = useContext(AccountContext)
+
   const [categories, setCategories] = useState<Category[]>([])
   const [error, setError] = useState('')
   const [reload, setReload] = useState(false)
@@ -23,7 +29,14 @@ export const Menu: React.FC<Props> = ({ listCategories }: Props) => {
   useEffect(() => {
     listCategories()
       .then(c => setCategories(c))
-      .catch(e => setError(e.message))
+      .catch(e => {
+        setError(e.message)
+        if (e instanceof UnauthorizedError) {
+          setCurrentAccount(undefined as any)
+
+          navigate('/login')
+        }
+      })
   }, [reload])
 
   return (
