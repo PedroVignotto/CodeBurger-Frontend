3ccd71aea8f674e4f00f13b320d571831a65eb20
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-13 15:09:01 -0300

    test: ensure Menu present categories list

--- tests/e2e/cypress/integration/menu.test.ts
+++ tests/e2e/cypress/integration/menu.test.ts
@@ -1,7 +1,8 @@
-import { mockServerError, mockUnauthorizedError } from '../mocks'
+import { mockOk, mockServerError, mockUnauthorizedError } from '../mocks'
 
 describe('Menu', () => {
   const mockError = (method: Function): void => method('GET', /categories/)
+  const mockSuccess = (): void => mockOk('GET', /categories/, 'categories-list')
 
   beforeEach(() => {
     cy.fixture('account').then(account => cy.setLocalStorageItem('account', account))
@@ -22,5 +23,16 @@ describe('Menu', () => {
     cy.visit('menu')
 
     cy.testUrl('/login')
+  })
+
+  it('Should present categories list', () => {
+    mockSuccess()
+
+    cy.visit('menu')
+
+    cy.get('ul').should('have.length', 2)
+    cy.get('li:empty').should('have.length', 7)
+    cy.get('ul').should('have.length', 3)
+    cy.get('li:not(:empty)').should('have.length', 6)
   })
 })
