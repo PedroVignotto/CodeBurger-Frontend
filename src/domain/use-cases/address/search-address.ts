221c17390efb7683b5d1f6659b8459f1e32d5c88
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-14 20:40:39 -0300

    feat: ensure SearchAddressUseCase throw FieldNotFound if HttpClient returns 400

--- src/domain/use-cases/address/search-address.ts
+++ src/domain/use-cases/address/search-address.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@/domain/contracts/http'
+import { FieldNotFoundError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient<{ district: string, street: string}>) => SearchAddress
 type Input = { zipCode: string }
@@ -6,5 +7,10 @@ type Output = void
 export type SearchAddress = (input: Input) => Promise<Output>
 
 export const searchAddressUseCase: Setup = (url, httpClient) => async ({ zipCode }) => {
-  await httpClient.request({ url: `${url}/${zipCode}`, method: 'get' })
+  const { statusCode } = await httpClient.request({ url: `${url}/${zipCode}`, method: 'get' })
+
+  switch (statusCode) {
+    case 200: break
+    default: throw new FieldNotFoundError('zipCode')
+  }
 }
