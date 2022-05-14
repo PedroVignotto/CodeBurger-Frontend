1dcf627799f7a86bf2b4ad3a43d35885a66cd305
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-14 20:47:03 -0300

    feat: ensure SearchAddressUseCase return address data if HttpClient returns 200

--- src/domain/use-cases/address/search-address.ts
+++ src/domain/use-cases/address/search-address.ts
@@ -3,14 +3,14 @@ import { FieldNotFoundError, UnauthorizedError, UnexpectedError } from '@/domain
 
 type Setup = (url: string, httpClient: HttpClient<{ district: string, street: string}>) => SearchAddress
 type Input = { zipCode: string }
-type Output = void
+type Output = { district: string, street: string}
 export type SearchAddress = (input: Input) => Promise<Output>
 
 export const searchAddressUseCase: Setup = (url, httpClient) => async ({ zipCode }) => {
-  const { statusCode } = await httpClient.request({ url: `${url}/${zipCode}`, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url: `${url}/${zipCode}`, method: 'get' })
 
   switch (statusCode) {
-    case 200: break
+    case 200: return data!
     case 400: throw new FieldNotFoundError('zipCode')
     case 401: throw new UnauthorizedError()
     default: throw new UnexpectedError()
