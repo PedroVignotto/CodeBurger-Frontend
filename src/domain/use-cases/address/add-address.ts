b9633c2d10a55c8246bdb64c8526c4fb75d5a5ee
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-14 20:27:25 -0300

    feat: ensure AddAddressUseCase return an address if HttpClient returns 201

--- src/domain/use-cases/address/add-address.ts
+++ src/domain/use-cases/address/add-address.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@/domain/contracts/http'
 import { UnauthorizedError, UnexpectedError } from '@/domain/errors'
+import { Address } from '@/domain/models'
 
-type Setup = (url: string, httpClient: HttpClient) => AddAddress
+type Setup = (url: string, httpClient: HttpClient<Address>) => AddAddress
 type Input = { surname: string, zipCode: string, district: string, street: string, number: number, complement?: string }
-type Output = void
+type Output = Address
 export type AddAddress = (input: Input) => Promise<Output>
 
 export const addAddressUseCase: Setup = (url, httpClient) => async (input) => {
-  const { statusCode } = await httpClient.request({ url, method: 'post', body: input })
+  const { statusCode, data } = await httpClient.request({ url, method: 'post', body: input })
 
   switch (statusCode) {
-    case 201: break
+    case 201: return data!
     case 401: throw new UnauthorizedError()
     default: throw new UnexpectedError()
   }
