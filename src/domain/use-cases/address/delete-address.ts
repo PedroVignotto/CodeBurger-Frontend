1b0ea1a46b6bc4685319928203d16d5ce7fa7036
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-14 16:00:22 -0300

    feat: ensure DeleteAddressUseCase throw UnexpectedError if HttpClient returns 400

--- src/domain/use-cases/address/delete-address.ts
+++ src/domain/use-cases/address/delete-address.ts
@@ -1,9 +1,15 @@
 import { HttpClient } from '@/domain/contracts/http'
+import { UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => DeleteAddress
 type Output = void
 export type DeleteAddress = () => Promise<Output>
 
 export const deleteAddressUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'delete' })
+  const { statusCode } = await httpClient.request({ url, method: 'delete' })
+
+  switch (statusCode) {
+    case 204: break
+    default: throw new UnexpectedError()
+  }
 }
