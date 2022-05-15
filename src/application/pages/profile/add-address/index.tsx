c4c6b7f1a47da6245eac7d3d972afe32ac340155
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-15 17:13:48 -0300

    feat: ensure AddAddress call validation with correct values

--- src/application/pages/profile/add-address/index.tsx
+++ src/application/pages/profile/add-address/index.tsx
@@ -1,20 +1,37 @@
 import { DefaultButton, Input, Spinner } from '@/application/components'
+import { Validator } from '@/application/validation'
 import { Default } from '@/application/layouts'
 
 import { Container } from './styles'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const AddAddress: React.FC = () => {
+type Props = { validation: Validator }
+
+export const AddAddress: React.FC<Props> = ({ validation }) => {
   const [formSearchVisible] = useState(true)
   const loading = false
+
+  const [zipCode, setZipCode] = useState('')
+  const [zipCodeError, setZipCodeError] = useState<string | undefined>('')
+  const [number, setNumber] = useState('')
+  const [numberError, setNumberError] = useState<string | undefined>('')
+  const [complement, setComplement] = useState('')
+  const [complementError, setComplementError] = useState<string | undefined>('')
+  const [surname, setSurname] = useState('')
+  const [surnameError, setSurnameError] = useState<string | undefined>('')
+
+  useEffect(() => setZipCodeError(validation.validate('zipCode', { zipCode })), [zipCode])
+  useEffect(() => setNumberError(validation.validate('number', { number })), [number])
+  useEffect(() => setComplementError(validation.validate('complement', { complement })), [complement])
+  useEffect(() => setSurnameError(validation.validate('surname', { surname })), [surname])
 
   return (
     <Default>
       <Container>
       {formSearchVisible
         ? <form data-testid="form-search">
-            <Input type="text" name="zipCode" placeholder="Informe seu CEP" state={''} setState={''} />
+            <Input type="text" name="zipCode" placeholder="Informe seu CEP" state={zipCodeError} setState={setZipCode} />
             <DefaultButton type="submit" disabled={true}>{loading ? <Spinner /> : 'Buscar'}</DefaultButton>
           </form>
         : <form data-testid="form-add">
@@ -24,10 +41,10 @@ export const AddAddress: React.FC = () => {
             </div>
             <div>
               <Input type="text" name="street" placeholder="Rua" state={''} setState={''} readOnly />
-              <Input type="text" name="number_address" placeholder="Número" state={''} setState={''} />
+              <Input type="text" name="number" placeholder="Número" state={numberError} setState={setNumber} />
             </div>
-            <Input type="text" name="complement" placeholder="Complemento" state={''} setState={''} />
-            <Input type="text" name="surname" placeholder="Apelido" state={''} setState={''} />
+            <Input type="text" name="complement" placeholder="Complemento" state={complementError} setState={setComplement} />
+            <Input type="text" name="surname" placeholder="Apelido" state={surnameError} setState={setSurname} />
             <DefaultButton type="submit">{loading ? <Spinner /> : 'Adicionar'}</DefaultButton>
           </form>
       }
