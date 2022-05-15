ba5949ecff4eb28c29cbc89434d5118a8218fdbd
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-15 16:56:52 -0300

    feat: ensure AddAddress load with correct initial state

--- src/application/pages/profile/add-address/index.tsx
+++ src/application/pages/profile/add-address/index.tsx
@@ -3,31 +3,34 @@ import { Default } from '@/application/layouts'
 
 import { Container } from './styles'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddAddress: React.FC = () => {
+  const [formSearchVisible] = useState(true)
   const loading = false
 
   return (
     <Default>
       <Container>
-      <form data-testid="form">
-        <Input type="text" name="zipCode" placeholder="Informe seu CEP" state={''} setState={''} />
-        <DefaultButton type="submit" >{loading ? <Spinner /> : 'Buscar'}</DefaultButton>
-      </form>
-      <form data-testid="form">
-        <div>
-          <Input type="text" name="district" placeholder="Bairro" state={''} setState={''} readOnly />
-          <Input type="text" name="zipCode" placeholder="CEP" state={''} setState={''} readOnly />
-        </div>
-        <div>
-          <Input type="text" name="street" placeholder="Rua" state={''} setState={''} readOnly />
-          <Input type="text" name="number_address" placeholder="Número" state={''} setState={''} />
-        </div>
-        <Input type="text" name="complement" placeholder="Complemento" state={''} setState={''} />
-        <Input type="text" name="surname" placeholder="Apelido" state={''} setState={''} />
-        <DefaultButton type="submit">{loading ? <Spinner /> : 'Adicionar'}</DefaultButton>
-      </form>
+      {formSearchVisible
+        ? <form data-testid="form-search">
+            <Input type="text" name="zipCode" placeholder="Informe seu CEP" state={''} setState={''} />
+            <DefaultButton type="submit" disabled={true}>{loading ? <Spinner /> : 'Buscar'}</DefaultButton>
+          </form>
+        : <form data-testid="form-add">
+            <div>
+              <Input type="text" name="district" placeholder="Bairro" state={''} setState={''} readOnly />
+              <Input type="text" name="zipCode" placeholder="CEP" state={''} setState={''} readOnly />
+            </div>
+            <div>
+              <Input type="text" name="street" placeholder="Rua" state={''} setState={''} readOnly />
+              <Input type="text" name="number_address" placeholder="Número" state={''} setState={''} />
+            </div>
+            <Input type="text" name="complement" placeholder="Complemento" state={''} setState={''} />
+            <Input type="text" name="surname" placeholder="Apelido" state={''} setState={''} />
+            <DefaultButton type="submit">{loading ? <Spinner /> : 'Adicionar'}</DefaultButton>
+          </form>
+      }
       </Container>
     </Default>
   )
