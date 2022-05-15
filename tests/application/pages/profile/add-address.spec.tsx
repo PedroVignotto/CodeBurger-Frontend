63bd8af98ce643c5e6865e58471865040cdcf411
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-15 17:14:08 -0300

    test: ensure AddAddress call validation with correct values

--- tests/application/pages/profile/add-address.spec.tsx
+++ tests/application/pages/profile/add-address.spec.tsx
@@ -1,14 +1,18 @@
 import { AddAddress } from '@/application/pages'
+import { Validator } from '@/application/validation'
 
 import { render, screen } from '@testing-library/react'
+import { mock } from 'jest-mock-extended'
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 
 describe('AddAddress', () => {
+  const validator = mock<Validator>()
+
   const makeSut = (): void => {
     render(
       <BrowserRouter>
-        <AddAddress />
+        <AddAddress validation={validator} />
       </BrowserRouter>
     )
   }
@@ -19,5 +23,14 @@ describe('AddAddress', () => {
     expect(screen.getByTestId('form-search')).toBeInTheDocument()
     expect(screen.queryByTestId('form-add')).not.toBeInTheDocument()
     expect(screen.getByRole('button', { name: /Buscar/i })).toBeDisabled()
+  })
+
+  it('Should call validation with correct values', () => {
+    makeSut()
+
+    expect(validator.validate).toHaveBeenCalledWith('zipCode', { zipCode: '' })
+    expect(validator.validate).toHaveBeenCalledWith('number', { number: '' })
+    expect(validator.validate).toHaveBeenCalledWith('complement', { complement: '' })
+    expect(validator.validate).toHaveBeenCalledWith('surname', { surname: '' })
   })
 })
