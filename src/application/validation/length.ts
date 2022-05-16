96b5dd6814e5f7a653b565dce5146e331f36fb53
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-15 21:49:49 -0300

    feat: ensure LengthValidation return falsy if value is valid

--- src/application/validation/length.ts
+++ src/application/validation/length.ts
@@ -5,6 +5,6 @@ export class LengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly length: number) {}
 
   validate (input: object): Error | undefined {
-    return new InvalidFieldError()
+    if ((input as any)[this.field]?.length !== this.length) return new InvalidFieldError()
   }
 }
