f08605e89067890b02e22fbe7adf8499b5469a6a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-16 00:18:08 -0300

    test: ensure ValidationBuilder return Length Validation if length() is call

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,14 +1,16 @@
-import { CompareValidation, EmailValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
+import { CompareValidation, EmailValidation, LengthValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
 
 import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   let fieldName: string
   let fieldToCompareName: string
+  let fieldLength: number
 
   beforeAll(() => {
     fieldName = faker.database.column()
     fieldToCompareName = faker.database.column()
+    fieldLength = faker.datatype.number()
   })
 
   it('Should return a Required validation if required() is call', () => {
@@ -23,9 +25,14 @@ describe('ValidationBuilder', () => {
     expect(validators).toStrictEqual([new EmailValidation(fieldName)])
   })
 
-  test('Should return CompareFieldsValidation', () => {
+  test('Should return Compare Fields Validation if sameAs() is call', () => {
     const validators = Builder.of(fieldName).sameAs(fieldToCompareName).build()
-    expect(validators).toEqual([new CompareValidation(fieldName, fieldToCompareName)])
+    expect(validators).toStrictEqual([new CompareValidation(fieldName, fieldToCompareName)])
+  })
+
+  test('Should return Length Validation if length() is call', () => {
+    const validators = Builder.of(fieldName).length(fieldLength).build()
+    expect(validators).toStrictEqual([new LengthValidation(fieldName, fieldLength)])
   })
 
   it('Should return a list of validations', () => {
