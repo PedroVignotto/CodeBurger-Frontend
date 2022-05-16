ed394be5ce9fa133b7fea021664c6c9bba4a0c7e
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-16 17:17:29 -0300

    feat: ensure UpdateAddressUseCase throw UnexpectedError if HttpClient returns 400

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@/domain/contracts/http'
+import { UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => UpdateAddress
 type Input = { id: string, surname?: string, number?: number, complement?: string }
@@ -6,5 +7,10 @@ type Output = void
 export type UpdateAddress = (input: Input) => Promise<Output>
 
 export const updateAddressUseCase: Setup = (url, httpClient) => async ({ id, ...input }) => {
-  await httpClient.request({ url: `${url}/${id}`, method: 'post', body: input })
+  const { statusCode } = await httpClient.request({ url: `${url}/${id}`, method: 'post', body: input })
+
+  switch (statusCode) {
+    case 204: break
+    default: throw new UnexpectedError()
+  }
 }
