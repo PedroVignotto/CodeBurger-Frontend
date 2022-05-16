1a1e51176cbc6cf139ae05e81ed89ed60753a595
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-15 22:24:09 -0300

    test: ensure RegisterAddress keep the button disabled if form is invalid

--- tests/e2e/cypress/integration/register-address.test.ts
+++ tests/e2e/cypress/integration/register-address.test.ts
@@ -1,4 +1,13 @@
+import faker from 'faker'
+
 describe('RegisterAddress', () => {
+  const validZipCode = faker.address.zipCode('########')
+  const invalidZipCode = faker.address.zipCode('####')
+
+  const populateSearchFormFields = (zipCode = validZipCode): void => {
+    cy.getInputById('zipCode').focus().type(zipCode)
+  }
+
   beforeEach(() => {
     cy.fixture('account').then(account => cy.setLocalStorageItem('account', account))
     cy.visit('address/register')
@@ -6,5 +15,12 @@ describe('RegisterAddress', () => {
 
   it('Should load with correct initial state', () => {
     cy.getSubmitButton().should('be.disabled').should('have.text', 'Buscar')
+  })
+
+  it('Should keep the button disabled if form is invalid', () => {
+    populateSearchFormFields(invalidZipCode)
+
+    cy.getInputById('zipCode').should('have.attr', 'title', 'Valor inválido')
+    cy.getSubmitButton().should('be.disabled')
   })
 })
