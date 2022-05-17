78f2a2568aaeb9f77c9d6df84f5deb604c0afad0
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 16:32:49 -0300

    fix: remove password validation on register address

--- src/main/factories/application/validation/register-address.ts
+++ src/main/factories/application/validation/register-address.ts
@@ -3,7 +3,6 @@ import { ValidationBuilder as Builder, ValidationComposite } from '@/application
 export const makeRegisterAddressValidation = (): ValidationComposite =>
   ValidationComposite.build([
     ...Builder.of('zipCode').required().length(8).build(),
-    ...Builder.of('password').required().build(),
     ...Builder.of('number').required().build(),
     ...Builder.of('surname').required().build()
   ])
