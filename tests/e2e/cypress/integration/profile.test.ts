18e14cc6373e5da466ea6e6b1f02eceded913fc0
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 22:51:38 -0300

    refactor: improve code quality

--- tests/e2e/cypress/integration/profile.test.ts
+++ tests/e2e/cypress/integration/profile.test.ts
@@ -3,213 +3,230 @@ import { mockNoContent, mockOk, mockServerError, mockUnauthorizedError } from '.
 import faker from 'faker'
 
 describe('Profile', () => {
-  const surname = faker.random.word()
-  const number = faker.datatype.number()
-  const complement = faker.random.words(3)
-
   const mockError = (method: Function): void => method('GET', /addresses/)
-  const mockSuccess = (): void => mockOk('GET', /addresses/, 'addresses-list')
-
-  const openEditModal = (): void => {
-    cy.getByTestId('details').click()
-    cy.getByTestId('edit').click()
-  }
-
-  const populateFields = (): void => {
-    openEditModal()
-    cy.getInputById('number').focus().clear().type(number.toString())
-    cy.getInputById('complement').focus().clear().type(complement)
-    cy.getInputById('surname').focus().clear().type(surname)
-  }
-
-  const simulateSubmit = (): void => {
-    populateFields()
-    cy.getSubmitButton().click()
-  }
+  const mockSuccess = (): void => mockOk('GET', /addresses/, 'address')
+  const visit = (): any => cy.visit('profile')
 
   beforeEach(() => {
     cy.fixture('account').then(account => cy.setLocalStorageItem('account', account))
   })
 
-  it('Should present error on UnexpectedError', () => {
-    mockError(mockServerError)
+  describe('List', () => {
+    it('Should present error on UnexpectedError', () => {
+      mockError(mockServerError)
 
-    cy.visit('profile')
+      visit()
 
-    cy.contains('Algo deu errado. Tente novamente!')
-    cy.get('button').contains('Tentar novamente')
+      cy.contains('Algo deu errado. Tente novamente!')
+      cy.get('button').contains('Tentar novamente')
+    })
+
+    it('Should reload on button click', () => {
+      mockError(mockServerError)
+
+      visit()
+      cy.contains('Tentar novamente').click()
+
+      cy.get('section').should('have.length', 1)
+    })
+
+    it('Should logout on UnauthorizedError', () => {
+      mockError(mockUnauthorizedError)
+
+      visit()
+
+      cy.testUrl('/login')
+    })
+
+    it('Should present addresses list', () => {
+      mockSuccess()
+
+      visit()
+
+      cy.get('section:empty').should('have.length', 1)
+      cy.get('section').should('have.length', 1)
+    })
+
+    it('Should logout when exit button is clicked', () => {
+      mockSuccess()
+
+      visit()
+      cy.get('button').contains('Sair').click()
+
+      cy.testUrl('/login')
+    })
+
+    it('Should go to add address page', () => {
+      mockSuccess()
+
+      visit()
+      cy.contains('Adicionar').click()
+
+      cy.testUrl('/address/register')
+    })
   })
 
-  it('Should reload on button click', () => {
-    mockError(mockServerError)
+  describe('delete', () => {
+    it('Should call delete address when delete button is clicked', () => {
+      mockSuccess()
+      mockNoContent('DELETE', /address/)
 
-    cy.visit('profile')
-    cy.contains('Tentar novamente').click()
+      visit()
+      cy.getByTestId('details').click()
+      cy.getByTestId('delete').click()
 
-    cy.get('section').should('have.length', 1)
+      cy.get('section:empty').should('have.length', 1)
+    })
+
+    it('Should prevent multiple requests on delete', () => {
+      mockSuccess()
+      mockNoContent('DELETE', /address/, 'deleteRequest')
+
+      visit()
+      cy.wait('@request')
+      cy.getByTestId('details').click()
+      cy.getByTestId('delete').dblclick()
+      cy.wait('@deleteRequest')
+
+      cy.get('@deleteRequest.all').should('have.length', 1)
+    })
+
+    it('Should present alert error if delete fails', () => {
+      mockSuccess()
+      mockServerError('DELETE', /address/, 'deleteRequest')
+
+      visit()
+      cy.wait('@request')
+      cy.getByTestId('details').click()
+      cy.getByTestId('delete').click()
+      cy.wait('@deleteRequest')
+
+      cy.contains('Algo deu errado. Tente novamente!')
+    })
+
+    it('Should logout if delete return UnauthorizedError', () => {
+      mockSuccess()
+      mockUnauthorizedError('DELETE', /address/, 'deleteRequest')
+
+      visit()
+      cy.wait('@request')
+      cy.getByTestId('details').click()
+      cy.getByTestId('delete').click()
+
+      cy.testUrl('/login')
+    })
   })
 
-  it('Should logout on UnauthorizedError', () => {
-    mockError(mockUnauthorizedError)
+  describe('edit', () => {
+    const surname = faker.random.word()
+    const number = faker.datatype.number()
+    const complement = faker.random.words(2)
 
-    cy.visit('profile')
+    const openEditModal = (): void => {
+      cy.getByTestId('details').click()
+      cy.getByTestId('edit').click()
+    }
 
-    cy.testUrl('/login')
-  })
+    const populateFields = (): void => {
+      openEditModal()
+      cy.getInputById('number').focus().clear().type(number.toString())
+      cy.getInputById('complement').focus().clear().type(complement)
+      cy.getInputById('surname').focus().clear().type(surname)
+    }
 
-  it('Should present addresses list', () => {
-    mockSuccess()
+    const simulateSubmit = (): void => {
+      populateFields()
+      cy.getSubmitButton().click()
+    }
 
-    cy.visit('profile')
+    it('Should load edit modal with correct initial state', () => {
+      mockSuccess()
 
-    cy.get('section:empty').should('have.length', 1)
-    cy.get('section').should('have.length', 1)
-  })
+      visit()
+      openEditModal()
 
-  it('Should logout when exit button is clicked', () => {
-    mockSuccess()
+      cy.getSubmitButton().should('be.enabled').should('have.text', 'Salvar')
+    })
 
-    cy.visit('profile')
-    cy.get('button').contains('Sair').click()
+    it('Should keep the button disabled if form is invalid', () => {
+      mockSuccess()
 
-    cy.testUrl('/login')
-  })
+      visit()
+      cy.wait('@request')
+      openEditModal()
+      cy.getInputById('number').focus().clear()
 
-  it('Should call delete address when delete button is clicked', () => {
-    mockSuccess()
-    mockNoContent('DELETE', /address/)
+      cy.getInputById('number').should('have.attr', 'title', 'Campo obrigatório')
+      cy.getSubmitButton().should('be.disabled')
+    })
 
-    cy.visit('profile')
-    cy.getByTestId('details').click()
-    cy.getByTestId('delete').click()
+    it('Should enable the button if form is valid', () => {
+      mockSuccess()
 
-    cy.get('section:empty').should('have.length', 1)
-  })
+      visit()
+      cy.wait('@request')
+      populateFields()
 
-  it('Should prevent multiple requests on delete', () => {
-    mockSuccess()
-    mockNoContent('DELETE', /address/, 'deleteRequest')
+      cy.getSubmitButton().should('be.enabled')
+    })
 
-    cy.visit('profile')
-    cy.getByTestId('details').click()
-    cy.getByTestId('delete').dblclick()
-    cy.wait('@deleteRequest')
+    it('Should present UnexpectedError on 500', () => {
+      mockSuccess()
+      mockServerError('PUT', /address/)
 
-    cy.get('@deleteRequest.all').should('have.length', 1)
-  })
+      visit()
+      cy.wait('@request')
+      simulateSubmit()
 
-  it('Should present alert error if delete fails', () => {
-    mockSuccess()
-    mockServerError('DELETE', /address/, 'deleteRequest')
+      cy.contains('Algo deu errado. Tente novamente!')
+    })
 
-    cy.visit('profile')
-    cy.getByTestId('details').click()
-    cy.getByTestId('delete').click()
-    cy.wait('@deleteRequest')
+    it('Should logout if edit return UnauthorizedError', () => {
+      mockSuccess()
+      mockUnauthorizedError('PUT', /address/)
 
-    cy.contains('Algo deu errado. Tente novamente!')
-  })
+      visit()
+      cy.wait('@request')
+      simulateSubmit()
 
-  it('Should logout if delete return UnauthorizedError', () => {
-    mockSuccess()
-    mockUnauthorizedError('DELETE', /address/, 'deleteRequest')
+      cy.testUrl('/login')
+    })
 
-    cy.visit('profile')
-    cy.getByTestId('details').click()
-    cy.getByTestId('delete').click()
+    it('Should prevent multiple submits', () => {
+      mockSuccess()
+      mockServerError('PUT', /address/, 'updateAddress')
 
-    cy.testUrl('/login')
-  })
+      visit()
+      cy.wait('@request')
+      populateFields()
+      cy.getSubmitButton().dblclick()
+      cy.wait('@updateAddress')
 
-  it('Should load edit modal with correct initial state', () => {
-    mockSuccess()
+      cy.get('@updateAddress.all').should('have.length', 1)
+    })
 
-    cy.visit('profile')
-    openEditModal()
+    it('Should not call submit if form is invalid', () => {
+      mockSuccess()
+      mockServerError('PUT', /address/, 'updateAddress')
 
-    cy.getSubmitButton().should('be.enabled').should('have.text', 'Salvar')
-  })
+      visit()
+      cy.wait('@request')
+      openEditModal()
+      cy.getInputById('number').focus().clear().type('{enter}')
 
-  it('Should keep the button disabled if form is invalid', () => {
-    mockSuccess()
+      cy.get('@updateAddress.all').should('have.length', 0)
+    })
 
-    cy.visit('profile')
-    openEditModal()
-    cy.getInputById('number').focus().clear()
+    it('Should close modal on success', () => {
+      mockSuccess()
+      mockNoContent('PUT', /address/, 'updateAddress')
 
-    cy.getInputById('number').should('have.attr', 'title', 'Campo obrigatório')
-    cy.getSubmitButton().should('be.disabled')
-  })
+      visit()
+      cy.wait('@request')
+      simulateSubmit()
+      cy.wait('@updateAddress')
 
-  it('Should enable the button if form is valid', () => {
-    mockSuccess()
-
-    cy.visit('profile')
-    populateFields()
-
-    cy.getSubmitButton().should('be.enabled')
-  })
-
-  it('Should present UnexpectedError on 500', () => {
-    mockSuccess()
-    mockServerError('PUT', /address/)
-
-    cy.visit('profile')
-    simulateSubmit()
-
-    cy.contains('Algo deu errado. Tente novamente!')
-  })
-
-  it('Should logout if edit return UnauthorizedError', () => {
-    mockSuccess()
-    mockUnauthorizedError('PUT', /address/)
-
-    cy.visit('profile')
-    simulateSubmit()
-
-    cy.testUrl('/login')
-  })
-
-  it('Should prevent multiple submits', () => {
-    mockSuccess()
-    mockServerError('PUT', /address/, 'updateAddress')
-
-    cy.visit('profile')
-    populateFields()
-    cy.getSubmitButton().dblclick()
-    cy.wait('@updateAddress')
-
-    cy.get('@updateAddress.all').should('have.length', 1)
-  })
-
-  it('Should not call submit if form is invalid', () => {
-    mockSuccess()
-    mockServerError('PUT', /address/, 'updateAddress')
-
-    cy.visit('profile')
-    openEditModal()
-    cy.getInputById('number').focus().clear().type('{enter}')
-
-    cy.get('@updateAddress.all').should('have.length', 0)
-  })
-
-  it('Should close modal on success', () => {
-    mockSuccess()
-    mockNoContent('PUT', /address/, 'updateAddress')
-
-    cy.visit('profile')
-    simulateSubmit()
-    cy.wait('@updateAddress')
-
-    cy.get('form').should('not.exist')
-  })
-
-  it('Should go to add address page', () => {
-    mockSuccess()
-
-    cy.visit('profile')
-    cy.contains('Adicionar').click()
-
-    cy.testUrl('/address/register')
+      cy.get('form').should('not.exist')
+    })
   })
 })
