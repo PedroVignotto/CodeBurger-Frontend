d7fa873ce1133e2c3120e35688062e748d84f0cb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-19 14:25:05 -0300

    feat: add cart context

---
+++ src/application/contexts/cart.tsx
@@ -0,0 +1,22 @@
+import { Product } from '@/domain/models'
+
+import React, { createContext, ReactNode, useState } from 'react'
+
+type Cart = { quantity: number, product: Product }
+
+type ContextProps = {
+  cart: Cart[]
+  addToCart: (product: Product) => void
+}
+
+export const CartContext = createContext<ContextProps>(null as any)
+
+type ProviderProps = { children: ReactNode }
+
+export function CartProvider ({ children }: ProviderProps): any {
+  const [cart, setCart] = useState<Cart[]>([])
+
+  const addToCart = (product: Product): void => setCart(old => ({ ...old, product }))
+
+  return <CartContext.Provider value={{ addToCart, cart }}>{children}</CartContext.Provider>
+}
--- src/main/router/index.tsx
+++ src/main/router/index.tsx
@@ -1,7 +1,7 @@
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters'
 import { PrivateRoute } from '@/main/proxies'
 import { GlobalStyle } from '@/application/styles'
-import { AccountContext } from '@/application/contexts'
+import { AccountContext, CartProvider } from '@/application/contexts'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
@@ -18,17 +18,19 @@ type Props = {
 export const Router: React.FC<Props> = ({ Login, SignUp, Menu, Profile, RegisterAddress }) => {
   return (
     <AccountContext.Provider value={{ setCurrentAccount: setCurrentAccountAdapter, getCurrentAccount: getCurrentAccountAdapter }}>
-      <ToastContainer autoClose={3000} theme="colored" />
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/menu" element={<PrivateRoute><Menu /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/address/register" element={<PrivateRoute><RegisterAddress /></PrivateRoute>} />
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <ToastContainer autoClose={3000} theme="colored" />
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/menu" element={<PrivateRoute><Menu /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/address/register" element={<PrivateRoute><RegisterAddress /></PrivateRoute>} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </AccountContext.Provider>
   )
 }
